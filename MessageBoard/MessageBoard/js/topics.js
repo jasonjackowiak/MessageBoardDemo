0645cd238f0184d1a82e5bc88d7ab06f4ee8bc73
jasonjackowiak <jason.jackowiak@gmail.com>
2015-07-26 21:08:46 +0930

    angular WIP
    adding in some client side validation and form data bound to controller

--- MessageBoard/MessageBoard/js/topics.js
+++ MessageBoard/MessageBoard/js/topics.js
@@ -9,6 +9,12 @@ thing.config(function ($routeProvider) {
             controllerAs: "app"
         }
     );
+    $routeProvider.when("/newmessage",
+    {
+        templateUrl: "/templates/newTopicView.html",
+        controller: "newTopicController",
+    });
+    $routeProvider.otherwise({ redirectTo: "/" });
 });
 
 function topicsController($scope, $http) {
@@ -34,27 +40,14 @@ function topicsController($scope, $http) {
         });
 };
 
+function newTopicController($scope, $http, $window) {
+    $scope.newTopicController = {};
+
+    $scope.save = function() {
+        alert($scope.newTopic.title);
+    }
+}
+
 //bind the controller to the function
 thing.controller('topicsController', topicsController);
-//.controller('topicsController', function topicsController($scope, $http) {
-//    $scope.data = [];
-//    $scope.isBusy = true;
-
-//    var self = this;
-//    self.message = "The app routing is working!";
-
-//    $scope.message = self.message;
-
-//    $http.get("api/v1/topics?IncludeReplies=true")
-//    .then(function (result) {
-//        //success
-//        angular.copy(result.data, $scope.data);
-//    },
-//        function () {
-//            //fail
-//            alert("FAIL!!");
-//        })
-//    .then(function () {
-//        $scope.isBusy = false;
-//    });
-//});
+thing.controller('newTopicController', newTopicController);
