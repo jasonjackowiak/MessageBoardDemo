fe6c1f613a791d39f8e2dad686c1eb724764616d
jasonjackowiak <jason.jackowiak@gmail.com>
2015-07-26 14:24:46 +0930

    angular WIP
    Still trying to get the MVC pattern working (for reading).
    Have got simple sample working, now to debug and apply properly...

--- MessageBoard/MessageBoard/js/home-index.js
+++ MessageBoard/MessageBoard/js/home-index.js
@@ -1,37 +1,38 @@
 ﻿//home index controller
 
-var module = angular.module('homeIndex', ['ngRoute']);
+var module = angular.module('MyApp', ['ngRoute']);
 
-module.config(function ($routeProvider) {
-    $routeProvider.when('/', {
-        controller: 'topicsController',
-        templateUrl: '/templates/topicsView.html'
-    });
-    $routeProvider.otherwise({ redirectTo: '/' });
-});
-
-function topicsController($scope, $http) {
+module.config(function($routeProvider) {
+        $routeProvider.when('/', {
+            controller: 'topicsController',
+            templateUrl: '/templates/topicsView.html'
+        });
+        $routeProvider.otherwise({ redirectTo: '/' });
+    }
+    .controller('topicsController', function topicsController($scope, $http) {
     $scope.data = [];
     $scope.isBusy = true;
 
     var self = this;
     self.message = "The app routing is working!";
 
+    $scope.message = self.message;
+
     $http.get("api/v1/topics?IncludeReplies=true")
-        .then(function (result) {
-            //success
-            angular.copy(result.data, $scope.data);
-        },
-            function () {
+        .then(function(result) {
+                //success
+                angular.copy(result.data, $scope.data);
+            },
+            function() {
                 //fail
                 alert("FAIL!!");
             })
-    .then(function () {
-        $scope.isBusy = false;
-    });
-}
+        .then(function() {
+            $scope.isBusy = false;
+        });
+}));
 
-angular.module("homeIndex", ['ngRoute']).controller('topicsController', topicsController);
-//module.$inject = ['$routeProvider'];
+//angular.module("homeIndex", ['ngRoute']).controller('topicsController', topicsController);
+module.$inject = ['$routeProvider'];
 ////this bit is required in new version of angular to hookup the controller
-//topicsController.$inject = ['$scope', '$http'];
+topicsController.$inject = ['$scope', '$http'];
---
+++ MessageBoard/MessageBoard/js/test.js
@@ -0,0 +1,27 @@
+﻿angular.module('MyApp', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider.when("/",
+            {
+                templateUrl: "/templates/topicsView.html",
+                controller: "topicsController",
+                controllerAs: "app"
+            }
+        );
+    })
+.controller('topicsController', function topicsController($scope, $http) {
+    $scope.data = [];
+    $scope.isBusy = true;
+
+    var self = this;
+    self.message = "The app routing is working!";
+
+    $scope.message = self.message;
+});
+
+//works
+//.controller('AppCtrl', function () {
+//    var self = this;
+//    self.message = "The app routing is working!";
+//});
+
+//works
