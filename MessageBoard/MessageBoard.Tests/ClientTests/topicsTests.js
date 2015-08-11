2f0afa9670cc48124c4bebe9497fa116b08bff4b
jasonjackowiak <jason.jackowiak@gmail.com>
2015-08-11 21:49:06 +0930

    Adding js unit tests
    failing atm

---
+++ MessageBoard/MessageBoard.Tests/ClientTests/topicsTests.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="c:\users\j\documents\github\messageboarddemo\messageboard\messageboard.tests\scripts\jasmine.js" />
+/// <reference path="../../messageboard/scripts/angular.js" />
+/// <reference path="../../messageboard/js/topics.js" />
+/// <reference path="../../messageboard/scripts/angular-mocks.js" />
+
+
+describe("topics Tests->", function () {
+
+    beforeEach(function() {
+        module('homeIndex', ['ngRoute']);
+    });
+
+    describe("dataService->", function() {
+
+        it("can load topics", inject(function(dataService) {
+            expect(dataService.topics).toEqual([]);
+        }));
+    });
+
+});
