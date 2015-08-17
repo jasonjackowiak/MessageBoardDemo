324b212baa8f0e67e60bd350608910aa2720512c
jasonjackowiak <jason.jackowiak@gmail.com>
2015-08-17 22:30:30 +0930

    last checkin

--- MessageBoard/MessageBoard.Tests/ClientTests/topicsTests.js
+++ MessageBoard/MessageBoard.Tests/ClientTests/topicsTests.js
@@ -1,7 +1,8 @@
 ﻿/// <reference path="c:\users\j\documents\github\messageboarddemo\messageboard\messageboard.tests\scripts\jasmine.js" />
 /// <reference path="../../messageboard/scripts/angular.js" />
-/// <reference path="../../messageboard/js/topics.js" />
+/// <reference path="../../messageboard/scripts/angular-route.js" />
 /// <reference path="../../messageboard/scripts/angular-mocks.js" />
+/// <reference path="../../messageboard/js/topics.js" />
 
 
 describe("topics Tests->", function () {
--- MessageBoard/MessageBoard/Data/MessageBoardContext.cs
+++ MessageBoard/MessageBoard/Data/MessageBoardContext.cs
@@ -14,6 +14,7 @@ namespace MessageBoard.Data
             this.Configuration.LazyLoadingEnabled = false;
             this.Configuration.ProxyCreationEnabled = false;
 
+            //System.Data.Entity.Database.SetInitializer(new CreateDatabaseIfNotExists<MessageBoardContext>());
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<MessageBoardContext, MessageBoardsMigrationConfiguration>());
         }
 
